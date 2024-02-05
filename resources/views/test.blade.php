e9b2dce9979d181c2e13c71e01ee1b35fbd4913f
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-02-05 14:24:23 +0700

    test tailwind

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,6 +9,6 @@
 </head>
 <body class="">
     <h1 class="underline text-5xl">Test</h1>
-    <button class="">Submit</button>
+    <button class="border border-black px-4 py-1 rounded bg-red-400 hover:bg-red-300 active:bg-red-500 mt-5">Submit</button>
 </body>
 </html>
