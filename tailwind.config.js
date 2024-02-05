31608e7e05162b886c3ae7f73fe633725e2b6478
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-02-05 15:08:16 +0700

    {Setup Daisy UI]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
+const customPalette = {
+    primary: "#474F7A",
+    secondary: "#FC6736",
+    "primary-content": "#ffffff",
+    "secondary-content": "#ffffff"
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -17,5 +24,25 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require("daisyui"),
+    ],
+
+    daisyui: {
+        themes: [
+            {
+                dark: {
+                    ...require("daisyui/src/theming/themes")["dark"],
+                    ...customPalette,
+                }
+            },
+            {
+                light: {
+                    ...require("daisyui/src/theming/themes")["light"],
+                    ...customPalette
+                }
+            }
+        ]
+    }
 };
