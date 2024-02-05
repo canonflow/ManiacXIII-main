12ad0a5bb3590d7dbd2aaa021ec62a661027aa20
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-02-05 14:21:53 +0700

    Add Breeze and Tailwind

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test Aja</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body class="">
+    <h1 class="underline text-5xl">Test</h1>
+    <button class="">Submit</button>
+</body>
+</html>
