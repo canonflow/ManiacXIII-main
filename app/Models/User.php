e17ef31a68ca61bafeb99c117f1940357fb10ef1
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-02-08 18:48:30 +0700

    Update Relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -50,7 +51,7 @@ class User extends Authenticatable
         return $this->hasMany(Participant::class, 'user_id');
     }
 
-    public function admins() : HasMany {
-        return $this->hasMany(Admin::class, 'user_id');
+    public function admin() : HasOne {
+        return $this->hasOne(Admin::class, 'user_id');
     }
 }
