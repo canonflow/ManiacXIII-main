29ab3ee7add218031c5e62b2225d0dfe215ae049
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-02-19 16:06:16 +0700

    [Admin] Refactor Route and Update Layout

--- resources/views/admin/layout/index.blade.php
+++ resources/views/admin/layout/index.blade.php
@@ -22,16 +22,10 @@
     @yield('styles')
 </head>
 <body>
-{{-- Button Open Side Panel --}}
-<button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-    <span class="sr-only">Open sidebar</span>
-    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-    </svg>
-</button>
+
 
 {{-- Side Panel --}}
-<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+<aside id="logo-sidebar" class="fixed top-0 left-0 z-50 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         {{-- Logo dan Judul --}}
         <a href="{{ route('admin.dashboard') }}" class="flex items-center ps-2.5 mb-5">
@@ -112,10 +106,19 @@
 {{-- Content --}}
 <div class="p-4 sm:ml-64">
     {{-- Navbar --}}
-    <div class="navbar bg-primary sticky top-3 rounded-md mb-4 px-4 z-40">
+    <div class="navbar bg-primary sticky top-3 rounded-md mb-4 px-4 z-40 justify-between">
         {{-- Page --}}
-        <div class="flex-1 text-2xl text-primary-content">
-            {{ $pageTitle ?? "Dashboard" }}
+        <div class="flex justify-center items-center gap-2">
+            {{-- Button Open Side Panel --}}
+            <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                <span class="sr-only">Open sidebar</span>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                </svg>
+            </button>
+            <div class="flex-1 text-2xl text-primary-content">
+                {{ $pageTitle ?? "Dashboard" }}
+            </div>
         </div>
 
         {{-- Profile --}}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\TeamController;
+//use App\Http\Controllers\Admin\AdminController;
+//use App\Http\Controllers\Admin\TeamController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,22 +29,22 @@ Route::group(
     ['middleware' => 'guest', 'prefix' => 'admin', 'as' => 'admin.'],
     function () {
         // Dashboard
-        Route::get('/', [AdminController::class, 'index'])->name('dashboard');
-        Route::get('/messages', [AdminController::class, 'messages'])->name('messages');
-        Route::get('/messages/search', [AdminController::class, 'searchMessage'])->name('search-message');
+        Route::get('/', [Admin\AdminController::class, 'index'])->name('dashboard');
+        Route::get('/messages', [Admin\AdminController::class, 'messages'])->name('messages');
+        Route::get('/messages/search', [Admin\AdminController::class, 'searchMessage'])->name('search-message');
         //Route::get('show', [AdminController::class, 'showMessages'])->name('show');
-        Route::get('/messages/{team:name}', [AdminController::class, 'showChat'])->name('chat');
-        Route::post('/message-send', [AdminController::class, 'sendChat'])->name('chat.send');
+        Route::get('/messages/{team:name}', [Admin\AdminController::class, 'showChat'])->name('chat');
+        Route::post('/message-send', [Admin\AdminController::class, 'sendChat'])->name('chat.send');
 
         // Registration
-        Route::get('/registration', [TeamController::class, 'index'])->name('teams.index');
-        Route::get('/registration/search', [TeamController::class, 'search'])->name('teams.search');
-        Route::post('registration/deactivate', [TeamController::class, 'deactivateTeam'])->name('teams.deactivate');
+        Route::get('/registration', [Admin\TeamController::class, 'index'])->name('teams.index');
+        Route::get('/registration/search', [Admin\TeamController::class, 'search'])->name('teams.search');
+        Route::post('registration/deactivate', [Admin\TeamController::class, 'deactivateTeam'])->name('teams.deactivate');
 
         // Uers
-        Route::get('/users', [UserController::class, 'index'])->name('users.index');
-        Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-        Route::post('/users/destroy', [UserController::class, 'destroy'])->name('users.destroy');
+        Route::get('/users', [Admin\UserController::class, 'index'])->name('users.index');
+        Route::post('/users/store', [Admin\UserController::class, 'store'])->name('users.store');
+        Route::post('/users/destroy', [Admin\UserController::class, 'destroy'])->name('users.destroy');
     }
 );
 
