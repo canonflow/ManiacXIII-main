06b8645b6b131bd2fdfdc294f49adbe8cc70b06d
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-02-26 16:27:59 +0700

    [Export] Add Styles

--- resources/views/admin/download/participant.blade.php
+++ resources/views/admin/download/participant.blade.php
@@ -9,33 +9,42 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <table class="">
+    <table class="" border="10" style="border: 1px solid black;">
         <thead>
             <tr>
-                <th valign="center" align="center"><strong>No</strong></th>
-                <th valign="center" align="center"><strong>School Name</strong></th>
-                <th valign="center" align="center"><strong>Header</strong></th>
-                <th valign="center" align="center"><strong>Ketua Tim</strong></th>
-                <th valign="center" align="center"><strong>Anggota 1</strong></th>
-                <th valign="center" align="center"><strong>Anggota 2</strong></th>
+                <th valign="center" align="center" style="background-color: #FEECE2; border: 1px solid black;"><strong>No</strong></th>
+                <th valign="center" align="center" style="background-color: #FEECE2; border: 1px solid black;"><strong>School Name</strong></th>
+                <th valign="center" align="center" style="background-color: #FEECE2; border: 1px solid black;"><strong>Header</strong></th>
+                <th valign="center" align="center" style="background-color: #FEECE2; border: 1px solid black;"><strong>Ketua Tim</strong></th>
+                <th valign="center" align="center" style="background-color: #FEECE2; border: 1px solid black;"><strong>Anggota 1</strong></th>
+                <th valign="center" align="center" style="background-color: #FEECE2; border: 1px solid black;"><strong>Anggota 2</strong></th>
             </tr>
         </thead>
         <tbody>
         @php($no = 1)
         @foreach($payloads as $school => $payload)
             @php($isFirst = true)
+            @php($headPos = 0)
             @foreach($payload as $header => $items)
                 <tr>
                     @if($isFirst)
-                        <td rowspan="5" valign="center" align="center">{{ $no }}</td>
-                        <td rowspan="5" valign="center" align="center">{{ $school }}</td>
+                        <td rowspan="5" valign="center" align="center" class="item" style="background-color: #C5EBAA; border: 1px solid black;"><strong>{{ $no }}</strong></td>
+                        <td rowspan="5" valign="center" align="center" class="item" style="background-color: #C5EBAA; border: 1px solid black;"><strong>{{ $school }}</strong></td>
                         @php($isFirst = false)
                     @endif
-                    <td>{{ $header }}</td>
-                    @foreach($items as $item)
-                        <td>{{ $item }}</td>
-                    @endforeach
+                    @if($headPos == 0)
+                        <td valign="center" align="center" style="background-color: #C5EBAA; border: 1px solid black;">{{ $header }}</td>
+                        @foreach($items as $item)
+                        <td valign="center" align="center" style="background-color: #C5EBAA; border: 1px solid black;">{{ $item }}</td>
+                        @endforeach
+                    @else
+                        <td valign="center" align="center" style="border: 1px solid black;">{{ $header }}</td>
+                        @foreach($items as $item)
+                            <td valign="center" align="center" style="border: 1px solid black;">{{ $item }}</td>
+                        @endforeach
+                    @endif
                 </tr>
+                @php($headPos++)
             @endforeach
             @php($no++)
         @endforeach
