95be3109e89477639d2aa14c61eb7d438057f822
fannyyrr29 <s160422005@student.ubaya.ac.id>
2024-02-28 20:25:51 +0700

    [Register Tim] Sisa insert ke DB

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Team;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Validation\ValidationException;  
 
 class RegisteredUserController extends Controller
 {
@@ -31,9 +33,31 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            //sekolah
+            'username' => ['required', 'string','min:8' ,'max:255', 'unique' .User::class],
+            'password' => ['required'],
+            'nama_tim' => ['required', 'string', 'max:255', 'unique:teams,name'],
+            'nama_sekolah' => ['required', 'string', 'max:255'],
+            'alamat_sekolah' => ['required', 'string', 'max:255'],
+            'nomor_sekolah' => ['required', 'string', 'max:255'],
+
+            //leader
+            'nama_leader' => ['required', 'string', 'max:255'],
+            'email_leader' => ['required', 'email', 'lowercase', 'max:255', 'unique:participants,email'],
+            'nomor_leader' => ['required', 'string', 'max:255', 'unique:participants,phone_number'],
+            'foto_leader' => ['required','file','max:10240', 'mimes:jpg,jpeg,png'],
+
+            //anggota1
+            'nama_anggota1' => ['required', 'string', 'max:255'],
+            'email_anggota1' => ['required', 'email', 'lowercase', 'max:255', 'unique:participants,email'],
+            'nomor_anggota1' => ['required', 'string', 'max:255', 'unique:participants,phone_number'],
+            'foto_anggota1' => ['required','file','max:10240', 'mimes:jpg,jpeg,png'],
+             
+            //anggota2
+            'nama_anggota2' => ['required', 'string', 'max:255'],
+            'email_anggota2' => ['required', 'email', 'lowercase', 'max:255', 'unique:participants,email'],
+            'nomor_anggota2' => ['required', 'string', 'max:255', 'unique:participants,phone_number'],
+            'foto_anggota2' => ['required','file','max:10240', 'mimes:jpg,jpeg,png'],
         ]);
 
         $user = User::create([
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +49,256 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registrasi</title>
+    {{-- @vite('resources/css/app/css') --}}
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+</head>
+
+<body>
+    <div class="container-fluid my-3">
+        <div class="container">
+            <h1 class="text-center">MANIAC XIII</h1>
+            <h1 class="text-center">REGISTRATION</h1>
+            <div class="form my-5">
+                <form class="row g-3 needs-validation" novalidate method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="col-md-12">
+                        <label for="validationCustomUsername" class="form-label">Username</label>
+                        <div class="input-group has-validation">
+                            {{-- <span class="input-group-text" id="inputGroupPrepend"> </span> --}}
+                            <input type="text" class="form-control @error('username') is-invalid @enderror"
+                                id="validationCustomUsername" aria-describedby="inputGroupPrepend" name="username"
+                                required placeholder="Masukkan Username">
+                            @error('username')
+                                <div class="invalid-feedback alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustomUsername" class="form-label">Password</label>
+                        <div class="input-group has-validation">
+                            {{-- <span class="input-group-text" id="inputGroupPrepend"> </span> --}}
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                id="validationCustomUsername" aria-describedby="inputGroupPrepend" required
+                                name="password" placeholder="Masukkan Password">
+                            @error('password')
+                                <div class="invalid-feedback alert-danger">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom01" class="form-label">Nama Tim</label>
+                        <input type="text" class="form-control @error('nama_tim') is-invalid @enderror"
+                            id="validationCustom01" placeholder="Masukkan Nama Tim" required name="nama_tim">
+                        @error('nama_tim')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom02" class="form-label">Nama Sekolah</label>
+                        <input type="text" class="form-control  @error('nama_sekolah') is-invalid @enderror"
+                            id="validationCustom02" placeholder="Masukkan Nama Sekolah" required name="nama_sekolah">
+                        @error('nama_sekolah')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Alamat Sekolah</label>
+                        <input type="text" class="form-control @error('alamat_sekolah') is-invalid @enderror"
+                            id="validationCustom03" placeholder="Masukkan Alamat Sekolah" required
+                            name="alamat_sekolah">
+                        @error('alamat_sekolah')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Nomor Telepon Sekolah</label>
+                        <input type="text" class="form-control @error('nomor_sekolah') is-invalid @enderror"
+                            id="validationCustom03" name="nomor_sekolah" required
+                            placeholder="Masukkan Nomor Telepon Sekolah">
+                        @error('nomor_sekolah')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <hr>
+                    {{-- Leader --}}
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Nama Ketua Tim</label>
+                        <input type="text" class="form-control @error('nama_leader') is-invalid @enderror"
+                            id="validationCustom03" required name="nama_leader">
+                        @error('nama_leader')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Email</label>
+                        <input type="text" class="form-control @error('email_leader') is-invalid @enderror"
+                            id="validationCustom03" required name="email_leader">
+                        @error('email_leader')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Nomor Telepon</label>
+                        <input type="text" class="form-control @error('nomor_leader') is-invalid @enderror"
+                            id="validationCustom03" required name="nomor_leader">
+                        @error('nomor_leader')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Foto</label>
+                        <input type="file" class="form-control @error('foto_leader') is-invalid @enderror"
+                            id="validationCustom03" required name="foto_leader">
+                        @error('foto_leader')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Alergi</label>
+                        <input type="text" class="form-control" id="validationCustom03" name="alergi_leader">
+                    </div>
+                    <hr>
+                    {{-- anggota 1 --}}
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Nama Anggota 1</label>
+                        <input type="text" class="form-control @error('nama_anggota1') is-invalid @enderror"
+                            id="validationCustom03" required name="nama_anggota1">
+                        @error('nama_anggota1')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Email</label>
+                        <input type="text" class="form-control @error('email_anggota1') is-invalid @enderror"
+                            id="validationCustom03" required name="email_anggota1">
+                        @error('email_anggota1')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Nomor Telepon</label>
+                        <input type="text" class="form-control @error('nomor_anggota1') is-invalid @enderror"
+                            id="validationCustom03" required name="nomor_anggota1">
+                        @error('nomor_anggota1')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Foto</label>
+                        <input type="file" class="form-control @error('foto_anggota1') is-invalid @enderror"
+                            id="validationCustom03" required name="foto_anggota1">
+                        @error('foto_anggota1')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Alergi</label>
+                        <input type="text" class="form-control" id="validationCustom03">
+                    </div>
+                    <hr>
+                    {{-- anggota 2 --}}
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Nama Anggota 2</label>
+                        <input type="text" class="form-control @error('nama_anggota2') is-invalid @enderror"
+                            id="validationCustom03" required name="anggota2_nama">
+                        @error('nama_anggota2')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Email</label>
+                        <input type="text" class="form-control @error('email_anggota2') is-invalid @enderror"
+                            id="validationCustom03" required name="email_anggota2">
+                        @error('email_anggota2')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Phone Number</label>
+                        <input type="text" class="form-control @error('nomor_anggota2') is-invalid @enderror"
+                            id="validationCustom03" required name="nomor_anggota2">
+                        @error('nomor_anggota2')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Photo</label>
+                        <input type="file" class="form-control @error('foto_anggota2') is-invalid @enderror"
+                            id="validationCustom03" required name="foto_anggota2">
+                        @error('nomor_anggota2')
+                            <div class="invalid-feedback alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-12">
+                        <label for="validationCustom03" class="form-label">Alergi</label>
+                        <input type="text" class="form-control" id="validationCustom03" name="alergi_anggota2">
+                    </div>
+                    <div class="col-12 my-3">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="invalidCheck"
+                                required>
+                            <label class="form-check-label" for="invalidCheck">
+                                Agree to terms and conditions
+                            </label>
+                            <div class="invalid-feedback">
+                                You must agree before submitting.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <button class="btn btn-primary" type="submit">Register</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
