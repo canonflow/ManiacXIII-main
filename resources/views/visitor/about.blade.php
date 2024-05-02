184a5fa0922991d3d9be7d3dff09474e4f58aa88
sariippp <abdurrahmanasgf88@gmail.com>
2024-05-02 23:54:34 +0700

    Compe & About us (blm fix)

--- resources/views/visitor/about.blade.php
+++ resources/views/visitor/about.blade.php
@@ -1,7 +1,146 @@
 @extends('visitor.welcome')
 
-@section('content')
-    <div class="content">
+@section('styles')
+    <style>
+        .container-bg {
+            background-image: url("{{ asset('asset2024/main/bg-transparent.png') }}");
+            background-repeat: repeat-y;
+            background-size: cover;
+        }
 
+        .box {
+            background-color: #a67563;
+            border-radius: 20px 20px 0 0;
+        }   
+
+        .carousel-image-wrapper {
+            padding: 40px; 
+            background-color: black; 
+            max-width: 60%; 
+            margin: 0 auto; 
+            border-radius: 20px;
+        }
+
+        .image-box {
+            width: auto;
+            height: auto;
+        }
+
+        h1{
+            font-family: Cinzel;
+            text-shadow: 1px 1px 1px #620706; 
+        }
+        
+        .body {
+            background-color: #7f4c42;
+            color: white;
+            border-radius: 0 0 20px 20px;
+            padding: 30px;
+            font-weight: 550;
+            text-align: center;
+            font-family: Montserrat;
+        }
+        /* Ukuran teks untuk layar kecil */
+        @media (max-width: 576px) {
+            .body {
+                font-size: 4vw !important; 
+            }
+        }
+
+        /* Ukuran teks untuk layar sedang */
+        @media (min-width: 577px) and (max-width: 768px) {
+            .body {
+                font-size: 2.5vw !important;
+            }
+        }
+
+        /* Ukuran teks untuk layar besar */
+        @media (min-width: 769px) {
+            .body {
+                font-size: 1.5vw !important ; 
+            }
+        }
+
+        .video-container {
+            background-color: #a67563;
+            padding: 20px;
+            border-radius: 20px;
+            max-width: 400px; /* Lebar maksimal untuk container video */
+            width: 100%; /* Agar responsif terhadap container induk */
+            display: flex;
+            justify-content: center; /* Untuk menjaga video tetap terpusat */
+        }
+
+        .video-container video {
+            max-width: 100%; /* Agar video tidak melewati lebar container */
+            height: auto; /* Mempertahankan rasio aspek */
+            border-radius: 20px; /* Menjaga sudut melengkung */
+        }
+    </style>
+@endsection
+
+@section('content')
+<div class="container-bg container-fluid">
+    <div class="container py-5">
+        <h1 class="text-center text-bold py-2">WHAT IS MANIAC</h1>
+        <br>
+        <div class="box"> 
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="carousel-image-wrapper d-flex justify-content-center"> 
+                                <img src="{{ asset('asset2024/main/shield.png') }}" class="rounded img-fluid" alt="Shield">
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="carousel-image-wrapper d-flex justify-content-center">
+                                <img src="{{ asset('asset2024/main/Shield.png') }}" class="rounded img-fluid" alt="Axe">
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="carousel-image-wrapper d-flex justify-content-center"> 
+                                <img src="{{ asset('asset2024/main/shield.png') }}" class="rounded img-fluid" alt="Sword">
+                            </div>
+                        </div>
+                    </div>
+                    <button 
+                        class="carousel-control-prev" 
+                        type="button" 
+                        data-bs-target="#carouselExampleIndicators" 
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button 
+                        class="carousel-control-next" 
+                        type="button" 
+                        data-bs-target="#carouselExampleIndicators" 
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+        </div> 
+        <div class="body fs-2">
+            MANIAC adalah acara lomba untuk anak SMA/K sederajat yang mencakup
+            workshop, rally games, dan final. Materi yang dilombakan mengenai Game
+            Concept Design dan Game Asset Design. MANIAC diselenggarakan oleh jurusan
+            Teknik Informatika program Digital Media Technology Universitas Surabaya. 
+        </div>
+        <br>
+        <h1 class="text-center text-bold py-2">JOIN NOW</h1>
+        <div class="container py-2 d-flex justify-content-center">
+            <div class="video-container">
+                <video controls>
+                    <source src="video-example.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
     </div>
+</div>
 @endsection
--- resources/views/visitor/competition.blade.php
+++ resources/views/visitor/competition.blade.php
@@ -1,124 +1,151 @@
 @extends('visitor.welcome')
 
 @section('styles')
-    <style>
+<style>
+    :root {
+    /* Variabel ukuran font untuk responsivitas */
+    --base-font-size: 16px; /* Ukuran dasar */
+    --font-large: 2em; /* Besar */
+    --font-medium: 1.5em; /* Sedang */
+    --font-small: 1em; /* Kecil */
+
+    /* Variabel warna */
+    --color-bg: #a67563;
+    --color-fg: white;
+    --color-primary: #590707;
+    --color-primary-dark: #741010;
+    }
+
+    .container-bg {
+        background-image: url("{{ asset('asset2024/main/bg-transparent.png') }}");
+        background-repeat: repeat-y;
+        background-size: cover;
+    }
+
+    .container-page {
+        padding-top: 3%;
+        text-align: center;
+    }
+
+    .box-container {
+        width: 70%;
+        margin: 0 auto;
+    }
+
+    .floating-box {
+        padding: 20px;
+    }
+
+    .sub {
+        background-color: var(--color-bg);
+        color: var(--color-fg);
+        border-radius: 20px 20px 0 0;
+        font-size: var(--font-large);
+        padding: 10px;
+        font-weight: bold;
+        text-align: center;
+        font-family: 'Montserrat';
+    }
+
+    .text {
+        background-color: #7f4c42;
+        color: var(--color-fg);
+        border-radius: 0 0 20px 20px;
+        padding: 30px;
+        text-align: justify;
+        font-family: 'Montserrat';
+        font-weight: bold;
+        font-size: var(--font-medium);
+    }
+
+    h1 {
+        font-family: 'Cinzel';
+        font-weight: bold;
+        text-align: center;
+        text-shadow: 1px 1px 1px #620706;
+    }
+
+    .button {
+        display: inline-block;
+        padding: 10px 60px;
+        background-color: var(--color-primary);
+        color: var(--color-fg);
+        text-decoration: none;
+        font-family: 'Montserrat';
+        font-weight: bold;
+        border-radius: 20px;
+        font-size: var(--font-medium);
+        border-bottom: 1px solid white;
+        transition: all 0.3s;
+    }
+
+    .button:hover {
+        background-color: var(--color-primary-dark);
+    }
+
+    @media (max-width: 768px) {
         .box-container {
-            width: 70%; 
-            margin: 0 auto; 
-        }
-
-        .floating-box {
-            padding: 20px; 
-        }
-
-        .sub {
-            background-color: #a67563;
-            color: white;
-            border-top-left-radius: 20px;
-            border-top-right-radius: 20px;
-            border-bottom-left-radius: 0px;
-            border-bottom-right-radius: 0px;
-            font-family: Montserrat;
-            font-weight: bold;
-            font-size: 36px;
-            text-align: center;
-            padding: 10px; 
-        }
-
-        .text {
-            background-color: #7f4c42;
-            color: white;
-            border-top-left-radius: 0px;
-            border-top-right-radius: 0px;
-            border-bottom-left-radius: 20px;
-            border-bottom-right-radius: 20px;
-            font-family: Montserrat;
-            text-align: justify;
-            padding: 10px; 
-        }
-
-        h1 {
-            font-family: Cinzel;
-            font-weight: bold;
-            text-align: center;
-            text-shadow: 1px 1px 1px #620706; 
+            width: 90%;
         }
 
         .button {
-            display: inline-block; 
-            padding: 10px 60px; 
-            background-color: blue; 
-            color: white; 
-            text-align: center; 
-            text-decoration: none; 
-            font-family: Montserrat;
-            font-weight: 500;
-            border-radius: 20px;
-            font-size: 36px;
-            margin-bottom: 20px;
+            padding: 8px 40px;
+            font-size: var(--font-medium);
+        }
+    }
+
+    @media (max-width: 480px) {
+        .box-container {
+            width: 100%;
         }
 
-        .centered {
-            text-align: center; 
+        .button {
+            padding: 6px 30px;
+            font-size: var(--font-small);
         }
-    </style>
+    }
+</style>
 @endsection
 
 @section('content')
-    <div> 
+<div class="container-bg container-fluid">
+    <div class="container-page">
         <h1>COMPETITIONS</h1>
         <br>
         <div class="box-container">
             <div class="floating-box">
-                <div class="sub">PENYISIHAN</div>
-                <div class="text">
-                    Babak penyisihan MANIAC XIII berupa workshop yang dibawakan langsung oleh
-                    salah satu game developer ternama di Indonesia. Terdapat dua jenis workshop
-                    yang diselenggarakan, yaitu Game Asset Design yang berfokus pada cara
-                    pembuatan aset di suatu permainan dan Game Concept Design yang membahas
-                    bagaimana cara merancang konsep sebuah permainan. Pada akhir workshop,
-                    peserta akan diberikan tugas pengumpulan karya yang akan dinilai dalam babak
-                    penyisihan. Dengan adanya workshop, diharapkan peserta dapat
-                    mengimplementasikan ilmu yang didapat dan bisa menjadi bekal ke babak final
-                    nantinya.
-                </div>
-                <br>
-                <div class="sub">SEMIFINAL</div>
-                <div class="text">
-                    Babak semi final MANIAC XIII akan diadakan di Fakultas Teknik Universitas
-                    Surabaya. Babak penyisihan akan terdiri dari Rally Games dan Game Besar yang
-                    wajib diikuti oleh setiap tim. Pada Rally Games akan terdapat pos-pos permainan
-                    yang harus diselesaikan dengan strategi dan kerja sama tim. Terdapat pula pos
-                    yang berisi pertanyaan teori mengenai game dan multimedia dasar, serta
-                    pertanyaan logika dan juga pos yang menguji kerja sama tim. Pada Game Besar,
-                    setiap tim akan bermain dan berkompetisi untuk memenangkan permainan.
-                </div>
-                <br>
-                <div class="sub">FINAL</div>
-                <div class="text">
+                @foreach (['PENYISIHAN', 'SEMIFINAL', 'FINAL'] as $section)
+                <div class="sub fs-1">{{ $section }}</div>
+                <div class="text fs-2">
+                    @if ($section == 'PENYISIHAN')
+                    Babak penyisihan MANIAC XIII berupa workshop yang dibawakan langsung oleh salah satu game developer ternama di Indonesia. Terdapat dua jenis workshop yang diselenggarakan, yaitu Game Asset Design yang berfokus pada cara pembuatan aset di suatu permainan dan Game Concept Design yang membahas bagaimana cara merancang konsep sebuah permainan. Peserta akan diberikan tugas pengumpulan karya pada akhir workshop.
+                    @elseif ($section == 'SEMIFINAL')
+                    Babak semi final MANIAC XIII akan diadakan di Fakultas Teknik Universitas Surabaya. Semi final terdiri dari Rally Games dan Game Besar yang harus diselesaikan dengan strategi dan kerja sama tim.
+                    @else
                     Babak Final MANIAC XIII akan diadakan secara on-site di Universitas Surabaya.
                     Topik yang akan dilombakan pada babak final adalah Game Concept Design dan
                     Game Asset Design.
-
+                    <br><br>
                     Game Concept Design adalah cabang lomba untuk membuat sebuah konsep
                     cerita atau alur dalam sebuah game. Peserta diharapkan dapat membuat
                     konsep game yang sesuai dengan tema.
-
+                    <br><br>
                     Game Asset Design adalah cabang lomba untuk mendesain aset-aset yang
                     dibutuhkan dalam game tersebut. Peserta diharapkan dapat mendesain aset
                     game semenarik mungkin sesuai dengan alur dan konsep yang telah dibuat.
                     Setiap tim akan mempresentasikan hasil kerjanya pada Babak Final. Peserta
                     harus bisa memberikan penjelasan mengenai konsep game beserta asetnya
                     yang menarik dan kreatif sesuai dengan tema yang ditentukan.
+                    @endif
                 </div>
                 <br>
+                @endforeach
             </div>
         </div>
         <br>
         <h1>GUIDEBOOK</h1>
         <br>
-        <div class="centered">
-            <a href="https://drive.google.com/" class="button" target="_blank">DOWNLOAD</a>
-        </div>
+        <a href="https://drive.google.com/" class="button btn-lg" target="_blank">DOWNLOAD</a>
     </div>
+</div>
 @endsection
