10de895097a3c372f6ea3f97671627bb4b0097d2
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-05-05 00:25:58 +0700

    Update Account

--- resources/views/visitor/welcome.blade.php
+++ resources/views/visitor/welcome.blade.php
@@ -135,13 +135,17 @@
                             <div class="dropdown">
                                 <button style="background-color: #7f4c42;"
                                     class="btn btn-secondary nav-link dropdown-toggle text-center" type="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    <strong><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                            fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                                            <path fill-rule="evenodd"
-                                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                                        </svg>&nbsp;ACCOUNT</strong>
+                                    data-bs-toggle="dropdown" aria-expanded="false"
+                                >
+                                    <strong>
+{{--                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"--}}
+{{--                                            fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">--}}
+{{--                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />--}}
+{{--                                            <path fill-rule="evenodd"--}}
+{{--                                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />--}}
+{{--                                        </svg>&nbsp;--}}
+                                        ACCOUNT
+                                    </strong>
                                 </button>
                                 <ul class="dropdown-menu">
                                     @auth
