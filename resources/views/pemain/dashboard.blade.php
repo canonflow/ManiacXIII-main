b1f8f18f7c65fe266f7a91a37e6853f0b52241fe
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-05-06 23:17:00 +0700

    [Pemain] Update Timeline

--- resources/views/pemain/dashboard.blade.php
+++ resources/views/pemain/dashboard.blade.php
@@ -148,29 +148,29 @@
                     <li class="h-32">
                         <hr/>
                         <div class="timeline-start text-sm mr-2">25 Juli 2024</div>
-                        <div class="timeline-middle badge badge-outline badge-md badge-base-content py-3">
+                        <div class="timeline-middle badge badge-outline badge-md badge-success py-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
                             </svg>
                         </div>
-                        <div class="timeline-end timeline-box rounded-lg">Penyisihan + TM Semifinal</div>
+                        <div class="timeline-end timeline-box rounded-lg">Penyisihan + TM Semi Final</div>
                         <hr/>
                     </li>
                     <li class="h-32">
                         <hr/>
                         <div class="timeline-start text-sm mr-2">29 Juli 2024</div>
-                        <div class="timeline-middle badge badge-outline badge-md badge-error py-3">
+                        <div class="timeline-middle badge badge-outline badge-md badge-success py-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 0 0 2.25-2.25V6.75a2.25 2.25 0 0 0-2.25-2.25H6.75A2.25 2.25 0 0 0 4.5 6.75v10.5a2.25 2.25 0 0 0 2.25 2.25Zm.75-12h9v9h-9v-9Z" />
                             </svg>
                         </div>
-                        <div class="timeline-end timeline-box rounded-lg">Semifinal + TM Final</div>
+                        <div class="timeline-end timeline-box rounded-lg">Semi Final + TM Final</div>
                         <hr />
                     </li>
                     <li class="h-32">
                         <hr/>
                         <div class="timeline-start text-sm mr-2">1 Agustus 2024</div>
-                        <div class="timeline-middle badge badge-outline badge-md badge-success py-3">
+                        <div class="timeline-middle badge badge-outline badge-md badge-base-content py-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
                             </svg>
@@ -204,7 +204,7 @@
             >
             <h1 class="text-md md:text-xl font-semibold bg-base-300 py-2 px-4 text-center rounded-md">Instruksi Penggunaan</h1>
             <div class="flex flex-col gap-3">
-                <div class="badge badge-accent rounded-md font-medium text-sm">6 Maret 2024</div>
+                <div class="badge badge-accent rounded-md font-medium text-sm">6 May 2024</div>
                 <div class="divider"></div>
                 <div>
                     <p class="p-0 pb-2 m-o font-bold">Akun</p>
